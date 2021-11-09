b7f4add7eebe73629e41b066b9b8ffd15524beb5
Davor Obilinovic <obee88@gmail.com>
2021-11-09 18:47:48 +0100

    Updates

---
+++ precache-manifest.8657b05b1e942381aa4fa9dab61758d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd119fbe2359a1247c4ca4e8b78c7b3d",
+    "url": "/world-stats/index.html"
+  },
+  {
+    "revision": "8eebbebc16f25c3837d2",
+    "url": "/world-stats/static/css/main.3f1416f7.chunk.css"
+  },
+  {
+    "revision": "190eae7a466a53d17292",
+    "url": "/world-stats/static/js/2.a2da2f6c.chunk.js"
+  },
+  {
+    "revision": "8eebbebc16f25c3837d2",
+    "url": "/world-stats/static/js/main.ee0a2e4e.chunk.js"
+  },
+  {
+    "revision": "c7f8d953ce6b1cfbe036",
+    "url": "/world-stats/static/js/runtime-main.0fabb687.js"
+  }
+]);
